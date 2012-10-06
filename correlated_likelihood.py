Fixed a bug in likelihood function and use sigma0 properly in covariance matrix.

correlated_likelihood.py
@@ -16,18 +16,19 @@ def correlated_gaussian_loglikelihood(xs, means, cov):
     
     ds=(xs-means)*sl.lu_solve((lu,piv), xs-means)/2.0
 
-    return -np.log(2.0*np.pi)*(ndim/2.0)-np.sum(np.log(lambdas))-np.sum(ds)
+    return -np.log(2.0*np.pi)*(ndim/2.0)-0.5*np.sum(np.log(lambdas))-np.sum(ds)
 
 def generate_covariance(ts, sigma0, tau):
     """Generates a covariance matrix according to an exponential
-    autocovariance: cov(t_i, t_j) = sigma0*exp(-|ti-tj|/tau)."""
+    autocovariance: cov(t_i, t_j) =
+    sigma0*sigma0*exp(-|ti-tj|/tau)."""
 
     ndim = ts.shape[0]
 
     tis = np.tile(np.reshape(ts, (-1, 1)), (1, ndim))
     tjs = np.tile(ts, (ndim, 1))
 
-    return sigma0*np.exp(-np.abs(tis-tjs)/tau)
+    return sigma0*sigma0*np.exp(-np.abs(tis-tjs)/tau)
 
 class LogPrior(object):
     """Log of the prior function."""